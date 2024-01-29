8083882de41dc30452731893e9721e38cc6ddc96
Yordan Vladov <jordan0vladov@gmail.com>
2024-01-29 22:06:49 +0200

    Created and Passed first test

---
+++ src/appointment/appointment.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+@Injectable()
+export class AppointmentService {
+  public scheduleAppointment(appointmentData: Record<string, any>) {
+    return {
+      ...appointmentData,
+      confirmed: false,
+    };
+  }
+}
