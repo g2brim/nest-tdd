561e528b01848ec8204287fc4f0bcbb32178c222
Yordan Vladov <jordan0vladov@gmail.com>
2024-01-30 22:28:27 +0200

    Bootsrapped module

--- src/appointment/appointment.model.ts
+++ src/appointment/appointment.model.ts
@@ -1,6 +1,16 @@
+import { Module } from "@nestjs/common";
+import { PatientModule } from "src/patient/patient.module";
+import { AppointmentService } from "./appointment.service";
+
 export interface Appointment{
     patientId: number;
     startTime: Date;
     endTime: Date;
     confirmed: boolean;
 }
+
+@Module({
+    imports: [PatientModule],
+    providers: [AppointmentService],
+  })
+  export class AppointmentModule {}
